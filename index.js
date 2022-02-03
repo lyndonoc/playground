5791ea3c897fa76b71026504ca4c31d335741172
lyndonoc <lyndonoc@gmail.com>
2022-02-02 20:59:40 -0500

    hello one

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log("hello world");
+<<<<<<< HEAD
 
 console.log("hello world");
 
+=======
+console.log("hello world");
+console.log("hello world");
+>>>>>>> 45633b2 (hello one)
 console.log("hello world");
