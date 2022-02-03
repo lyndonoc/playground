abbf9c39d1e90faabfb17eb54235f6cf39ec97aa
lyndonoc <lyndonoc@gmail.com>
2022-02-02 21:07:00 -0500

    world one

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log("hello world");
 console.log("hello world");
 
 function worldworld() {}
+
+console.log("hello world");
+console.log("hello world");
+
+function helloWorld() {
+  console.log("hello world");
+}
