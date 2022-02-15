68d032bfecbe230c3f1675458fc8635a7913c2cd
lyndonoc <lyndonoc@gmail.com>
2022-02-15 11:24:04 -0500

    four

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ console.log("hello world");
 console.log("hello world");
 console.log("hello world");
 console.log("hello world");
-
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
