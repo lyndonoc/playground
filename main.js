0ea4dd59fde7dcd12609dd62672c0bffc447e0e8
lyndonoc <lyndonoc@gmail.com>
2022-07-04 17:34:05 -0400

    one

--- main.js
+++ main.js
@@ -385,12 +385,3 @@ console.log("hello world");
 console.log("hello world");
 
 console.log("renamed to main");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-
-console.log("renamed to main");
