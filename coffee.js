ffe5701c7fc16417e488b8ec8248e87347f7becb
lyndonoc <lyndonoc@gmail.com>
2022-07-20 20:52:44 -0400

    one

--- coffee.js
+++ coffee.js
@@ -297,16 +297,3 @@ function hello() {}
 function hello() {}
 function hello() {}
 function hello() {}
-function getCoffee() {
-  return "coffee";
-}
-
-function getMoreCoffee() {
-  return "more coffee";
-}
-
-function hello() {}
-function hello() {}
-function hello() {}
-function hello() {}
-function hello() {}
